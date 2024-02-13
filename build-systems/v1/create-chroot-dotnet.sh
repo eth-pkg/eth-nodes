dcd4d45edfbf56e662a44fc87fb1d8ba3a030523
Eniko Nagy <4188977+eenagy@users.noreply.github.com>
2024-02-13 14:43:07 +0700

    feat: dotnet sbuild env, nethermind builds correctly

--- build-systems/v1/create-chroot-dotnet.sh
+++ build-systems/v1/create-chroot-dotnet.sh
@@ -8,6 +8,8 @@ fi
 CODENAME="$1"
 ARCH="$2"
 CHROOT_PREFIX="$CODENAME-$ARCH-dotnet"
+SRV_PREFIX=/srv/chroot/"$CHROOT_PREFIX"
+
 # Clean up previous chroots
 sudo rm -rf "/etc/sbuild/chroot/$CHROOT_PREFIX"
 sudo rm -rf "/etc/schroot/chroot.d/${CHROOT_PREFIX}"*
@@ -17,11 +19,13 @@ sudo rm -rf "/srv/chroot/$CHROOT_PREFIX"
 sudo sbuild-createchroot --merged-usr \
                          --include="ca-certificates" \
                          --chroot-prefix="$CHROOT_PREFIX" \
-                         "$CODENAME" "/srv/chroot/$CHROOT_PREFIX" \
+                         "$CODENAME" "$SRV_PREFIX" \
                          http://deb.debian.org/debian
 
 # Download and install Microsoft package
 wget https://packages.microsoft.com/config/debian/12/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo cp packages-microsoft-prod.deb "/srv/chroot/$CHROOT_PREFIX/root/"
-sudo chroot "/srv/chroot/$CHROOT_PREFIX" dpkg -i /root/packages-microsoft-prod.deb
-sudo rm "/srv/chroot/$CHROOT_PREFIX/root/packages-microsoft-prod.deb"
+sudo cp packages-microsoft-prod.deb "$SRV_PREFIX/root/"
+sudo chroot "$SRV_PREFIX" dpkg -i /root/packages-microsoft-prod.deb
+sudo rm "$SRV_PREFIX/root/packages-microsoft-prod.deb"
+sudo chroot "$SRV_PREFIX" apt-get update
+sudo chroot "$SRV_PREFIX" apt install -y dotnet-sdk-7.0
