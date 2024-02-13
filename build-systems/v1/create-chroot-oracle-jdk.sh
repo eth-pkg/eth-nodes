fc3d410902d2452978e0313d07a3afe91fc5c72d
Eniko Nagy <4188977+eenagy@users.noreply.github.com>
2024-02-13 14:14:20 +0700

    jdk is broken, saving change still

--- build-systems/v1/create-chroot-oracle-jdk.sh
+++ build-systems/v1/create-chroot-oracle-jdk.sh
@@ -18,22 +18,30 @@ sudo rm -rf "/srv/chroot/$CHROOT_PREFIX"
 
 # Create new chroot
 sudo sbuild-createchroot --merged-usr \
-                         --include="ca-certificates" \
+                         --include="ca-certificates wget apt-utils mlocate" \
                          --chroot-prefix="$CHROOT_PREFIX" \
                          "$CODENAME" "/srv/chroot/$CHROOT_PREFIX" \
                          http://deb.debian.org/debian
 
 
 sudo chroot $SRV_PREFIX /bin/bash -c '
-wget https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.deb
-sudo apt install ./jdk-17_linux-x64_bin.deb
-
-cat <<EOF | sudo tee /etc/profile.d/jdk.sh
-export JAVA_HOME=/usr/lib/jvm/jdk-17/
+java -version
+wget https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.tar.gz
+tar -zxf jdk-17_linux-x64_bin.tar.gz
+mkdir -p /opt/lib/jvm
+mkdir -p /usr/lib/jvm
+mv jdk-17.0.10 /opt/lib/jvm/jdk-17-oracle-x64
+ls /opt/lib/jvm/jdk-17-oracle-x64
+ln -s /opt/lib/jvm/jdk-17-oracle-x64  /usr/lib/jvm/jdk-17
+cat <<EOF | tee /etc/profile.d/jdk.sh
+export JAVA_HOME=/usr/lib/jvm/jdk-17
 export PATH=\$PATH:\$JAVA_HOME/bin
 EOF
 
-
+mkdir -p /var/lib/plocate
+updatedb
+locate libjli.so
 source /etc/profile.d/jdk.sh
-sudo ln -s /usr/lib/jvm/jdk-17-oracle-x64  /usr/lib/jvm/jdk-17
+/opt/lib/jvm/jdk-17-oracle-x64/bin/java -version
+java -version
 '
