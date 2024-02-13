ae0538f93a864a087b18e88e6a8fd8607cb37027
Eniko Nagy <4188977+eenagy@users.noreply.github.com>
2024-02-13 11:35:17 +0700

    feat: eth-node-lodestar building

--- build-systems/v1/create-chroot-node.sh
+++ build-systems/v1/create-chroot-node.sh
@@ -8,7 +8,7 @@ fi
 CODENAME="$1"
 ARCH="$2"
 CHROOT_PREFIX="$CODENAME-$ARCH-node"
-# NODESOURCE_REPO_URL="deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/nodesource.gpg] https://deb.nodesource.com/node_21.x nodistro main"
+SRV_PREFIX=/srv/chroot/"$CHROOT_PREFIX"
 
 # Clean up previous chroots
 sudo rm -rf "/etc/sbuild/chroot/$CHROOT_PREFIX"
@@ -17,19 +17,13 @@ sudo rm -rf "/srv/chroot/$CHROOT_PREFIX"
 
 # Create new chroot
 sudo sbuild-createchroot --merged-usr \
-                         --include="ca-certificates" \
+                         --include="apt-transport-https ca-certificates curl gnupg" \
                          --chroot-prefix="$CHROOT_PREFIX" \
-                         "$CODENAME" "/srv/chroot/$CHROOT_PREFIX" \
+                         "$CODENAME" "$SRV_PREFIX" \
                          http://deb.debian.org/debian
 
-# # Configure NodeSource repository
-# echo "$NODESOURCE_REPO_URL" | sudo tee "/srv/chroot/$CHROOT_PREFIX/etc/apt/sources.list.d/nodesource.list" > /dev/null
-# curl -fsSL "https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key" | gpg --dearmor | sudo tee "/srv/chroot/$CHROOT_PREFIX/etc/apt/trusted.gpg.d/nodesource.gpg" > /dev/null
-
-# # Configure package preferences
-# echo -e "Package: nsolid\nPin: origin deb.nodesource.com\nPin-Priority: 600" | sudo tee "/srv/chroot/$CHROOT_PREFIX/etc/apt/preferences.d/nsolid" > /dev/null
-# echo -e "Package: nodejs\nPin: origin deb.nodesource.com\nPin-Priority: 600" | sudo tee "/srv/chroot/$CHROOT_PREFIX/etc/apt/preferences.d/nodejs" > /dev/null
 
 # Install Node.js in the chroot environment
-sudo chroot "/srv/chroot/$CHROOT_PREFIX" /bin/bash -c 'curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash - && sudo apt-get install -y nodejs'
+# Should be working but is not
+sudo chroot "/srv/chroot/$CHROOT_PREFIX" /bin/bash -c 'curl -fsSL https://deb.nodesource.com/setup_lts.x | bash - && apt-get install -y nodejs'
 sudo chroot "/srv/chroot/$CHROOT_PREFIX" /bin/bash -c 'npm install --global yarn'
