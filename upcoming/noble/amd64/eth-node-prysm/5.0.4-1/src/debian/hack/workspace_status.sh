a774e10c738f5f9acc3ecac7e4dfc890632b667e
Eniko Nagy <4188977+eenagy@users.noreply.github.com>
2024-06-28 10:53:58 +0200

    fix: version information

---
+++ upcoming/noble/amd64/eth-node-prysm/5.0.4-1/src/debian/hack/workspace_status.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# action_env through bazel does not work https://github.com/bazelbuild/bazel/issues/8578
+# providing env variables also does not work
+# patching each update is not feasible on each update, neither automatable
+
+echo STABLE_GIT_COMMIT "38f208d70dc95b12c08403f5c72009aaa10dfe2f"
+echo DATE "2024-04-30 01:44:55+00:00"
+echo DATE_UNIX "1714441523"
+echo DOCKER_TAG "5.0.4"
+echo STABLE_GIT_TAG "5.0.4"
