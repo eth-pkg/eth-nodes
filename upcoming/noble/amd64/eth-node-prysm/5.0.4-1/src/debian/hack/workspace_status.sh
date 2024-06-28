0989576c8613ced0257bd7fd461c45be2c1b7768
Eniko Nagy <4188977+eenagy@users.noreply.github.com>
2024-06-28 13:50:07 +0200

    update git commit

--- upcoming/noble/amd64/eth-node-prysm/5.0.4-1/src/debian/hack/workspace_status.sh
+++ upcoming/noble/amd64/eth-node-prysm/5.0.4-1/src/debian/hack/workspace_status.sh
@@ -4,7 +4,7 @@
 # providing env variables also does not work
 # patching each update is not feasible on each update, neither automatable
 
-echo STABLE_GIT_COMMIT "38f208d70dc95b12c08403f5c72009aaa10dfe2f"
+echo STABLE_GIT_COMMIT "3b184f43c86baf6c36478f65a5113e7cf0836d41"
 echo DATE "2024-04-30 01:44:55+00:00"
 echo DATE_UNIX "1714441523"
 echo DOCKER_TAG "5.0.4"
