4af9eefb84c87bc2bf586f0f1fb50af3b03b51c7
Eniko Nagy <4188977+eenagy@users.noreply.github.com>
2024-09-11 16:46:51 +0200

    refactor: test package

--- upcoming/noble/amd64/eth-node-besu-testnet/0.0.1-1/src/debian/scripts/run-besu-service.sh
+++ upcoming/noble/amd64/eth-node-besu-testnet/0.0.1-1/src/debian/scripts/run-besu-service.sh
@@ -2,5 +2,5 @@
 
 
 exec /usr/lib/eth-node-besu-testnet/bin/run-besu.sh \
-    --conf-file /etc/eth-node-testnet/conf.d/testnet.conf \
+    --conf-file /etc/eth-node-testnet-config/testnet.conf \
     --conf-file /etc/eth-node-besu-testnet/besu-testnet.conf
--- upcoming/noble/amd64/eth-node-teku-testnet/0.0.1-1/src/debian/scripts/run-teku-service.sh
+++ upcoming/noble/amd64/eth-node-teku-testnet/0.0.1-1/src/debian/scripts/run-teku-service.sh
@@ -2,5 +2,5 @@
 
 
 exec /usr/lib/eth-node-teku-testnet/bin/run-teku.sh \
-    --conf-file /etc/eth-node-testnet/conf.d/testnet.conf \
+    --conf-file /etc/eth-node-testnet-config/testnet.conf \
     --conf-file /etc/eth-node-teku-testnet/teku-testnet.conf
