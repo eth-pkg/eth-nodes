4d6abf5b6e5f8469f202f54b40fb3f8814c98ac6
Eniko Nagy <4188977+eenagy@users.noreply.github.com>
2024-09-12 09:50:08 +0200

    fix: create file if not exists

--- upcoming/noble/amd64/eth-node-testnet-config/0.0.1-1/src/debian/scripts/postprocess.sh
+++ upcoming/noble/amd64/eth-node-testnet-config/0.0.1-1/src/debian/scripts/postprocess.sh
@@ -5,13 +5,17 @@ get_debconf_value() {
     local package=$1
     local key=$2
     
-    sudo debconf-show "$package" | grep "$key" | awk -F ': ' '{print $2}'
+    debconf-show "$package" | grep "$key" | awk -F ': ' '{print $2}'
 }
 
 
 package=eth-node-testnet-config
 value=BASE_CONFIG_SECRETS_FILE
 jwt_file=$(get_debconf_value $package $package/$value)
+
+if [ -z "$jwt_file" ];then 
+    jwt_file="/etc/eth-node-testnet/jwt.hex"
+fi 
 
 if [ ! -f "$jwt_file" ] || [ ! -s "$jwt_file" ]; then
     openssl rand -hex 32 | tr -d "\n" | tee "$jwt_file" >/dev/null
