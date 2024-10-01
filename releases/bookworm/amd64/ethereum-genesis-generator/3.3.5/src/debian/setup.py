3d8e733922b72aef77ec48c3a5257159ddbea9bb
Eniko Nagy <4188977+eenagy@users.noreply.github.com>
2024-10-01 10:03:39 +0700

    chore: cxFreeze is not reproducible

--- releases/bookworm/amd64/ethereum-genesis-generator/3.3.5/src/debian/setup.py
+++ releases/bookworm/amd64/ethereum-genesis-generator/3.3.5/src/debian/setup.py
@@ -2,12 +2,16 @@ import cx_Freeze
 from cx_Freeze import Executable
 
 # Dependencies can be automatically detected, but it might need fine-tuning.
-build_options = {"packages": [], "excludes": []}
+build_options = {
+    "packages": ["web3", "ruamel.yaml",],
+    "excludes": [],
+    "includes": ["os", "sys", "encodings", "web3", "ruamel.yaml"],
+}
 
 executables = [
     Executable("genesis_besu.py", target_name="genesis_besu"),
     Executable("genesis_chainspec.py", target_name="genesis_chainspec"),
-    Executable("genesis_geth.py", target_name="genesis_geth")
+    Executable("genesis_geth.py", target_name="genesis_geth"),
 ]
 
 cx_Freeze.setup(
@@ -15,5 +19,5 @@ cx_Freeze.setup(
     version="3.3.5",
     description="Genesis tools for blockchain configuration",
     options={"build_exe": build_options},
-    executables=executables
+    executables=executables,
 )
