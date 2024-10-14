2eb87e7add14d2dd96e8bc39612be21dc21f53d7
Eniko Nagy <4188977+eenagy@users.noreply.github.com>
2024-10-14 13:01:17 +0700

    chore: nethermind with all the options

--- releases/noble/all/eth-node-nethermind-regtest/0.0.1-1/src/debian/scripts/postprocess.sh
+++ releases/noble/all/eth-node-nethermind-regtest/0.0.1-1/src/debian/scripts/postprocess.sh
@@ -20,6 +20,9 @@ mkdir -p /var/lib/eth-node-regtest/nethermind
 echo "Setting ownership of /var/lib/eth-node-regtest/nethermind to eth-node-nethermind-regtest"
 chown -R eth-node-nethermind-regtest:eth-node-nethermind-regtest /var/lib/eth-node-regtest/nethermind
 
+mkdir -p /var/logs/eth-node-regtest/nethermind 
+chown -R eth-node-nethermind-regtest:eth-node-nethermind-regtest /var/logs/eth-node-regtest/nethermind
+
 echo "Adding eth-node-nethermind-regtest to eth-node-regtest group"
 usermod -aG eth-node-regtest eth-node-nethermind-regtest || true 
 
