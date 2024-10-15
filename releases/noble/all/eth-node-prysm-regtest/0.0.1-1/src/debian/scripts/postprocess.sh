0cca3d35f1a9a4d05ca59197bcf6654178d6a3be
Eniko Nagy <4188977+eenagy@users.noreply.github.com>
2024-10-15 16:14:34 +0700

    chore: prysm with all the options provided

--- releases/noble/all/eth-node-prysm-regtest/0.0.1-1/src/debian/scripts/postprocess.sh
+++ releases/noble/all/eth-node-prysm-regtest/0.0.1-1/src/debian/scripts/postprocess.sh
@@ -20,6 +20,9 @@ mkdir -p /var/lib/eth-node-regtest/prysm
 echo "Setting ownership of /var/lib/eth-node-regtest/prysm to eth-node-prysm-regtest"
 chown -R eth-node-prysm-regtest:eth-node-prysm-regtest /var/lib/eth-node-regtest/prysm
 
+mkdir -p /var/logs/eth-node-regtest/prysm 
+chown -R eth-node-prysm-regtest:eth-node-prysm-regtest /var/logs/eth-node-regtest/prysm
+
 echo "Adding eth-node-prysm-regtest to eth-node-regtest group"
 usermod -aG eth-node-regtest eth-node-prysm-regtest || true 
 
