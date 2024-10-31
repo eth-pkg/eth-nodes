373f4903743418b30093000d911102b0190f4501
Eniko Nagy <4188977+eenagy@users.noreply.github.com>
2024-10-31 14:29:23 +0700

    feat: erigon remove scripts

--- upcoming/noble/all/eth-node-erigon-regtest/0.0.2-1/src/debian/scripts/run-erigon-service.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash 
-
-
-exec /usr/lib/eth-node-erigon-regtest/bin/run-erigon.sh \
-    --conf-file /etc/eth-node-regtest-config/regtest.conf \
-    --conf-file /etc/eth-node-erigon-regtest/erigon-regtest.conf
