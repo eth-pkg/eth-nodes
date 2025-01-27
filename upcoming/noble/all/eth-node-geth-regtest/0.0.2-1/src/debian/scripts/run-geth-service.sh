93dd6d2098ace81b285d3606b2d5fef60c78db99
Eniko Nagy <4188977+eenagy@users.noreply.github.com>
2025-01-27 15:04:57 +0100

    chore: add geth config file

--- upcoming/noble/all/eth-node-geth-regtest/0.0.2-1/src/debian/scripts/run-geth-service.sh
+++ upcoming/noble/all/eth-node-geth-regtest/0.0.2-1/src/debian/scripts/run-geth-service.sh
@@ -3,4 +3,4 @@
 
 exec /usr/lib/eth-node-geth-regtest/bin/run-geth.sh \
     --conf-file /etc/eth-node-regtest-config/regtest.conf \
-    --conf-file /etc/eth-node-geth-regtest/geth-regtest.conf
+    --conf-file /etc/eth-node-geth-regtest/eth-node-geth-regtest.conf
