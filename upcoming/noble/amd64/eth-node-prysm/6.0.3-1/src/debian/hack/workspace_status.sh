880f344a066dbe5045bd37267a2de9402cda0863
Eniko Nagy <4188977+eenagy@users.noreply.github.com>
2025-05-22 09:46:15 +0200

    upcoming/noble/amd64/eth-node-prysm/6.0.3-1

---
+++ upcoming/noble/amd64/eth-node-prysm/6.0.3-1/src/debian/hack/workspace_status.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# action_env through bazel does not work https://github.com/bazelbuild/bazel/issues/8578
+# providing env variables also does not work
+# patching each update is not feasible on each update, neither automatable
+
+echo STABLE_GIT_COMMIT "eafef8c7c803f5a63755a9a63377c3bd6459aa41"
+echo DATE "2025-05-22 09:45:15+00:00"
+echo DATE_UNIX "1747899915"
+echo DOCKER_TAG "v6.0.3"
+echo STABLE_GIT_TAG "v6.0.3"
